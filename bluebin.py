0a7e836a0201c9ba0e3dbfc767867274070e0743
fjij <30779570+fjij@users.noreply.github.com>
2020-02-11 19:17:30 -0500

    Finished initial component parsing, added example file to test with.

--- bluebin.py
+++ bluebin.py
@@ -19,6 +19,7 @@ class InvalidArgumentException(Exception):
 
 class IncorrectArgumentCountException(Exception):
     pass
+        
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Functions                                                                   #
@@ -38,13 +39,59 @@ def parse_args():
 def show_help():
     print('\nUsage: python3 bluebin.py [INPUT FILE] [OUTPUT FILE]\n')
 
+def is_code(line):
+    return line.startswith('    ') or line.startswith('\t')
+
+def is_lid(line):
+    return not is_code(line) and line.strip().startswith('[') and line.strip().endswith(']')
+
+def lid_name(line):
+    return line[1:-1].strip()
+
+def create_component(name):
+    return {'name': name, 'properties': {}, 'content': ''}
+
+def is_property(line):
+    prev = ''
+    for c in line:
+        if c == ':' and prev != '\\':
+            return True
+    return False
+
+def parse_property(line):
+    return list(map(lambda s: s.strip(), line.split(':', 1)))
+
+def identify_components(in_str):
+    components = [create_component('__root__')]
+    reading_properties = False
+    for line in in_str.splitlines():
+        if is_lid(line):
+            components.append(create_component(lid_name(line)))
+            reading_properties = True
+        elif reading_properties and is_property(line):
+            key, value = parse_property(line)
+            components[-1]['properties'][key] = value
+        elif line.strip() == '':
+            reading_properties = False
+        else:
+            reading_properties = False
+            components[-1]['content'] += line + '\n'
+    return components
+
+def bluebin(in_str):
+    components = identify_components(in_str)
+    print(components)
+
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Main                                                                        #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 if __name__ == '__main__':
     try:
-        parse_args()
+        input_file, output_file = parse_args()
+        with open(input_file, 'r') as f:
+            bluebin(f.read())
     except Exception as e:
         print('\nERROR: ' + str(e))
         show_help()
---
+++ example.bb
@@ -0,0 +1,12 @@
+This is an example bluebin file!
+
+[My Component]
+x: 1
+y: 2
+
+I have %x% apples and %y% oranges!
+
+[My Other Component]
+123: abc
+
+It's easy as %123%!
